f49f6c8b86de98f4ebc496a7fa98b80cc546fc6b
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-03-09 13:14:02 -0300

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+
+
+function App() {
+
+  return (
+    <SafeAreaView style={Styles.Page}>
+      <StatusBar hidden={true} />
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+}
+
+const Styles = StyleSheet.create({
+  Page : {
+    flex : 1,
+  }
+});
+
+export default App;
